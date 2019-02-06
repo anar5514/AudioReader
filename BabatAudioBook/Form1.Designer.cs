1f4e8cbe61303631928946743b133b39fa5a8a34
Anar Axmed <anar.axmed5514@gmail.com>
2019-02-06 22:03:13 +0400

    Last!

--- BabatAudioBook/Form1.Designer.cs
+++ BabatAudioBook/Form1.Designer.cs
@@ -28,13 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.listView = new System.Windows.Forms.ListView();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.Location = new System.Drawing.Point(401, 12);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(90, 56);
+            this.btnLeft.TabIndex = 1;
+            this.btnLeft.Text = "<";
+            this.btnLeft.UseVisualStyleBackColor = true;
+            // 
+            // btnRight
+            // 
+            this.btnRight.Location = new System.Drawing.Point(685, 13);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(90, 56);
+            this.btnRight.TabIndex = 1;
+            this.btnRight.Text = ">";
+            this.btnRight.UseVisualStyleBackColor = true;
+            // 
+            // listView
+            // 
+            this.listView.AllowDrop = true;
+            this.listView.Location = new System.Drawing.Point(13, 13);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(353, 383);
+            this.listView.TabIndex = 3;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.SelectedIndexChanged += new System.EventHandler(this.listView_SelectedIndexChanged);
+            this.listView.DragDrop += new System.Windows.Forms.DragEventHandler(this.listView_DragDrop);
+            this.listView.DragEnter += new System.Windows.Forms.DragEventHandler(this.listView_DragEnter);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(161, 402);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(403, 47);
+            this.btnPlay.TabIndex = 4;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox.Location = new System.Drawing.Point(385, 75);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(403, 321);
+            this.pictureBox.TabIndex = 5;
+            this.pictureBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.listView);
+            this.Controls.Add(this.btnRight);
+            this.Controls.Add(this.btnLeft);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.Button btnRight;
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.PictureBox pictureBox;
     }
 }
 
--- BabatAudioBook/Form1.cs
+++ BabatAudioBook/Form1.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Speech.Synthesis;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +16,44 @@ namespace BabatAudioBook
 {
     public partial class Form1 : Form
     {
+        public string Filename { get; set; }
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void listView_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+        }
+
+        private void listView_DragDrop(object sender, DragEventArgs e)
+        {
+            listView.Items.Add(e.Data.ToString());
+            var data = e.Data.GetData(DataFormats.FileDrop, false) as string[];
+            Filename = data[0];
+
+        }
+
+        private void listView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            PdfReader pdfReader = new PdfReader(Filename);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 1; i < pdfReader.NumberOfPages ; i++)
+            {
+                sb.Append(PdfTextExtractor.GetTextFromPage(pdfReader, i));
+            }
+
+            var a = new SpeechSynthesizer();
+            a.SelectVoiceByHints(VoiceGender.Neutral);
+            a.Speak(sb.ToString());
+            
+            pdfReader.Close();
         }
     }
 }
